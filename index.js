bb9990e880a1460bf0709cfa91cbc20fb3b47aac
Cristian Collazos Prado <cristian.cabuntu@gmail.com>
2020-03-31 14:49:55 -0500

    mi Primer commit

---
+++ index.js
@@ -0,0 +1,40 @@
+/*const http = require("http");
+
+const colors = require("colors");
+
+const handleServer = function(req, res){
+    res.writeHead(200, {'content-type':'text/html'})
+    res.write("<h1>Hola mundo!</h1>");
+    res.end();
+}
+
+const server = http.createServer(handleServer);
+
+server.listen(30000, function(){
+    console.log("Server on port 3000".green);
+});*/
+
+/*const fs = require('fs');
+
+fs.writeFile("./texto.txt", "Linea 1", function(err){
+    if(err){
+        console.log(err);
+    }
+    else{
+        console.log("archivo creado!");
+    }
+    
+});console.log(substract(1,2));
+console.log(multiple(1,2));
+console.log(divide(1,2));*/
+const colors = require("colors");
+const express = require("express");
+
+const server = express();
+
+server.get("/", function(req, res){
+    res.send("<h1>Hola mundo desde express y nodejs</h1>");
+});
+server.listen(3000, function(){
+    console.log("Server express 3000".red);
+});
